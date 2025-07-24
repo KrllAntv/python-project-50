8badfb3604943b23e6279e7e344bbb964a242613
krll.antv <antv.krll@gmail.com>
2025-07-24 22:30:16 +0300

    update test

--- tests/test_data/test_generate_diff.py
+++ tests/test_data/test_generate_diff.py
@@ -6,12 +6,16 @@ def read_file_test():
         data = file.read()
         return data
 
+def read_empty_file():
+    return '{\n  \n}'
+
 def test_generate_diff():
     assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == read_file_test()
-
+    assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.yaml') == read_file_test()
+    assert generate_diff('tests/test_data/empty_file1.json', 'tests/test_data/empty_file2.json') == read_empty_file()
 
 
 def test_unsupport_format():
     with pytest.raises(ValueError):
-        generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.yaml')
+        generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.xml') #XML unsupported format 
     assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == read_file_test()
