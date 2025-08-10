2d81d37ec3eaba3d99cf49fe32f2027ea068ca9d
krll.antv <antv.krll@gmail.com>
2025-08-10 13:52:56 +0300

    update files

--- tests/test_data/test_generate_diff.py
+++ tests/test_data/test_generate_diff.py
@@ -5,17 +5,22 @@ def read_file_test():
     with open('tests/test_data/expectation_diff.txt', 'r') as file:
         data = file.read()
         return data
+    
+def read_file_test2():
+    with open('tests/test_data/expect_diff.txt', 'r') as file:
+        data = file.read()
+        return data    
 
 def read_empty_file():
-    return '{\n  \n}'
+    return '{\n}\n'
 
 def test_generate_diff():
     assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == read_file_test()
     assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.yaml') == read_file_test()
+    assert generate_diff('tests/test_data/test_file1.json', 'tests/test_data/test_file2.json') == read_file_test2()
     assert generate_diff('tests/test_data/empty_file1.json', 'tests/test_data/empty_file2.json') == read_empty_file()
 
 
 def test_unsupport_format():
     with pytest.raises(ValueError):
-        generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.xml') #XML unsupported format 
-    assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == read_file_test()
+        generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.xml') #XML unsupported format
