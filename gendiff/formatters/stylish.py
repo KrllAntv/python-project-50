e7f7beba42de3b5e254b3ad54a93a3552044349e
krll.antv <antv.krll@gmail.com>
2025-08-14 16:05:18 +0300

    update code

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,34 +1,55 @@
-def is_bool(arg):
-    if arg is False or arg is True:
+def is_value(arg):
+    if isinstance(arg, dict):
+        res = []
+        for k, v in arg.items():
+            res.append({'key': k, 'status': 'nested', 'value': is_value(v)})
+        return res
+    elif isinstance(arg, bool):
         return str(arg).lower()
     elif arg is None:
         return 'null'
     else:
-        return str(arg)
-    
+        return arg
+
+
+SIGN = {
+    'add': '+', 'delete': '-', 
+    'unchange': ' ', 'old_value': '-', 
+    'new_value': '+', 'nested': ' ',
+}
+
 
 def stylish_format(data, repl=' ', space_count=4, _lvl=1):
-    SIGN = {
-        'add': '+', 'delete': '-', 
-        'unchange': ' ', 'old_value': '-', 
-        'new_value': '+', 'nested': ' ',
-    }
     res = '{\n'
     ind = repl * ((space_count * _lvl) - 2)
     for item in data:
         if isinstance(item.get('value'), list):
+            # Рекурсивный обход если ключ value - список
             value = stylish_format(item['value'], repl, space_count, _lvl + 1)
-            res += f'{ind}{SIGN[item['status']]} {item['key']}: {value}' + '\n'
-        elif isinstance(item.get('value'), dict):
-            chang_dict = [
-                {
-                    'key': k, 'status': 'unchange', 'value': v
-                }
-            for k, v in item['value'].items()]
-            value = stylish_format(chang_dict, repl, space_count, _lvl + 1)
-            res += f'{ind}{SIGN[item['status']]} {item['key']}: {value}' + '\n'
+            res += f'{ind}{SIGN[item['status']]} {item['key']}: {value}'
+            res += '\n'  
+        elif item['status'] == 'change':
+            # Вывод словаря со статусом "CHANGE"
+            val1 = is_value(item['old_value'])
+            val2 = is_value(item['new_value'])
+            res += f'{ind}{SIGN['old_value']} {item['key']}: ' 
+            res += f'{
+                val1 if not isinstance(val1, list)
+                else stylish_format(val1, repl, space_count, _lvl + 1)
+                }'
+            res += '\n'
+            res += f'{ind}{SIGN['new_value']} {item['key']}: ' 
+            res += f'{
+                val2 if not isinstance(val2, list) 
+                else stylish_format(val2, repl, space_count, _lvl + 1)}'
+            res += '\n'
         else:
+            # Вывод остальных словарей
             res += f'{ind}{SIGN[item['status']]} {item['key']}: '
-            res += f'{is_bool(item['value'])}' + '\n'
+            val = is_value(item['value'])
+            res += f'{
+                val if not isinstance(val, list) 
+                else stylish_format(val, repl, space_count, _lvl + 1)}'
+            res += '\n'
     res += f'{repl * space_count * (_lvl - 1)}' + '}'
     return res
