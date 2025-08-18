36e6a21474c85fdf48514a2b8eafddf8f9c1bffc
krll.antv <antv.krll@gmail.com>
2025-08-18 22:48:50 +0300

    add func

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,9 +1,12 @@
 from gendiff.formatters.stylish import stylish_format
 from gendiff.generate_diff import sorted_diff
 from gendiff.parser import read_diff
+from gendiff.formatters.plain import plain_format
+
 
 __all__ = (
     read_diff,
     stylish_format,
     sorted_diff,
+    plain_format,
 )
