6d937d1809ad5420b1a52e2acbeaf63a5984d9b6
krll.antv <antv.krll@gmail.com>
2025-08-24 23:37:41 +0300

    change file

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,12 +1,15 @@
+from gendiff.formatters.plain import plain_format
 from gendiff.formatters.stylish import stylish_format
+from gendiff.formatters.json import json_format
+from gendiff.formatters.formatter import format
 from gendiff.generate_diff import sorted_diff
 from gendiff.parser import read_diff
-from gendiff.formatters.plain import plain_format
-
 
 __all__ = (
     read_diff,
     stylish_format,
     sorted_diff,
     plain_format,
+    json_format,
+    format
 )
