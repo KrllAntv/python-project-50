e547b18f82557cabd9870a5b1ad7a6e7454c5201
krll.antv <antv.krll@gmail.com>
2025-08-26 07:11:14 +0300

    change tests

--- tests/test_data/test_generate_diff.py
+++ tests/test_data/test_generate_diff.py
@@ -11,17 +11,40 @@ def read_empty_file():
     return '{\n}'
 
 def test_generate_diff():
-    assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == read_file_test('tests/test_data/expectation_diff.txt')
-    assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.yaml') == read_file_test('tests/test_data/expectation_diff.txt')
-    assert generate_diff('tests/test_data/test_file1.json', 'tests/test_data/test_file2.json') == read_file_test('tests/test_data/expect_diff.txt')
-    assert generate_diff('tests/test_data/empty_file1.json', 'tests/test_data/empty_file2.json') == read_empty_file()
+    assert generate_diff(
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.json'
+        ) == read_file_test('tests/test_data/expectation_diff.txt')
+    assert generate_diff(
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.yaml'
+        ) == read_file_test('tests/test_data/expectation_diff.txt')
+    assert generate_diff(
+        'tests/test_data/test_file1.json',
+        'tests/test_data/test_file2.json'
+        ) == read_file_test('tests/test_data/expect_diff.txt')
+    assert generate_diff(
+        'tests/test_data/empty_file1.json',
+        'tests/test_data/empty_file2.json'
+        ) == read_empty_file()
 
 
 def test_unsupport_format():
     with pytest.raises(ValueError):
-        generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.xml') #XML unsupported format
+        generate_diff(
+            'tests/test_data/file1.json',
+            'tests/test_data/file2.xml'
+            ) # XML unsupported format
 
 
 def test_plain():
-    assert generate_diff('tests/test_data/test_file1.json', 'tests/test_data/test_file2.json', 'plain') == read_file_test('tests/test_data/test_plain.txt')
-    assert generate_diff('tests/test_data/test_file1.json', 'tests/test_data/test_file2.yaml', 'plain') == read_file_test('tests/test_data/test_plain.txt')
+    assert generate_diff(
+        'tests/test_data/test_file1.json',
+        'tests/test_data/test_file2.json',
+        'plain'
+        ) == read_file_test('tests/test_data/test_plain.txt')
+    assert generate_diff(
+        'tests/test_data/test_file1.json',
+        'tests/test_data/test_file2.yaml',
+        'plain'
+        ) == read_file_test('tests/test_data/test_plain.txt')
